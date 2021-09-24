8667148a20429f156beac34bf1d8aa478a30e8e9
Can Soylu <hsyncn17@gmail.com>
2021-09-24 16:56:33 +0300

    useQuery

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
+import { useQuery } from "react-query";
 import "./App.css";
 
+const fetchUsers = async () => {
+  try {
+    const res = await fetch("https://reqres.in/api/users");
+    const user = await res.json();
+    return user;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
 function App() {
+  //grab all users
+
+  const { data: users, isLoading, error } = useQuery("users", fetchUsers);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    <p>something went wrong...</p>;
+  }
+
+  console.log(users);
+
   return (
     <div className="App">
-      <p>React query is awesome!!</p>
+      {users.data.map((user) => (
+        <p key={user.id}>
+          {user.first_name} {user.last_name}
+        </p>
+      ))}
     </div>
   );
 }
